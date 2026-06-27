7c7b5929bf5539926d5c7a3c89eed6e54c58eba6
Yan Shicheng <ikubeops@gmail.com>
2026-06-27 15:06:24 +0800

    fix: 创建项目时同步到 DevOps

--- application/portal-rpc/internal/logic/portalprojectservice/createProjectLogic.go
+++ application/portal-rpc/internal/logic/portalprojectservice/createProjectLogic.go
@@ -7,6 +7,8 @@ import (
 	"github.com/yanshicheng/kube-nova/application/portal-rpc/pb"
 	"github.com/yanshicheng/kube-nova/common/handler/errorx"
 
+	devopspb "github.com/yanshicheng/kube-nova/application/devops-manager-rpc/pb"
+
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
@@ -36,6 +38,22 @@ func (l *CreateProjectLogic) CreateProject(in *pb.PortalCreateProjectReq) (*pb.P
 	}
 
 	l.Infof("创建项目成功，ID: %d, UUID: %s, Name: %s", project.Id, project.Uuid, project.Name)
+
+	// 同步到 DevOps
+	if l.svcCtx.DevopsManagerRpc != nil {
+		syncReq := &devopspb.DevopsSyncProjectInfoReq{
+			PortalProjectUuid: project.Uuid,
+			Name:              project.Name,
+			Description:       in.Description,
+		}
+		_, syncErr := l.svcCtx.DevopsManagerRpc.SyncProjectInfo(l.ctx, syncReq)
+		if syncErr != nil {
+			l.Errorf("同步项目信息到 DevOps 失败: %v", syncErr)
+		} else {
+			l.Infof("同步项目到 DevOps 成功，UUID: %s", project.Uuid)
+		}
+	}
+
 	return &pb.PortalCreateProjectResp{
 		Id:   project.Id,
 		Uuid: project.Uuid,
